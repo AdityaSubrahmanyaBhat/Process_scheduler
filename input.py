691e689d24dea74eefa6432225f85931bf6e9ee6
AdityaSubrahmanyaBhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2022-01-14 12:34:20 +0530

    added gif and removed lottie

--- input.py
+++ input.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from streamlit_lottie import st_lottie
 import numpy as np
 import pandas as pd
+from PIL import Image
+import base64
 import algos
-import lottie
 
 algorithms=["FCFS - First come first serve","SJF - Shortest Job First", "SRTF - Shortest Remaining Time First"]
 
@@ -14,7 +14,10 @@ def input_form():
         bt=st.text_input('Burst Time')
         res=st.form_submit_button('Calculate')
 
-    if at=='' or bt=='' and res:
+    if at==''and  res:
+        st.error("Input cannot be empty")
+
+    elif bt=='' and res:
         st.error("Input cannot be empty")
     
     elif bt.split(" ").count('0')>0 and res:
@@ -24,8 +27,13 @@ def input_form():
         st.warning("Number of arrival times and burst times do not match")
 
     elif res==False:
-        lottie_json = lottie.load_lottieurl('https://assets4.lottiefiles.com/packages/lf20_hk63stcp.json')
-        st_lottie(lottie_json)
+        file_ = open("maths.gif", "rb")
+        contents = file_.read()
+        data_url = base64.b64encode(contents).decode("utf-8")
+        file_.close()
+
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="">',unsafe_allow_html=True,)
 
     elif at!='' and bt!='' and res:
         at_list=list(map(int,at.split(" ")))
